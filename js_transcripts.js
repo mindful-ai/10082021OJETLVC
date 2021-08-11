724b2cc46277a9cebbf652ff7ab13d1a8ba2728c
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2021-08-11 13:34:12 +0530

    Add files via upload

--- js_transcripts.js
+++ js_transcripts.js
@@ -485,3 +485,31 @@ ReferenceError: pick is not defined
 }
 
 
+
+
+
+> while (i < 11){
+    ... console.log(5 + ' X ' + i + ' = ' + 5 * i);
+    ... i++;
+    ... }
+    5 X 0 = 0
+    5 X 1 = 5
+    5 X 2 = 10
+    5 X 3 = 15
+    5 X 4 = 20
+    5 X 5 = 25
+    5 X 6 = 30
+    5 X 7 = 35
+    5 X 8 = 40
+    5 X 9 = 45
+    5 X 10 = 50
+    10
+    > i
+    11
+    > do {
+    ... console.log(5 + ' X ' + i + ' = ' + 5 * i);
+    ... i++;
+    ... } while (i < 10);
+    5 X 11 = 55
+    11
+    >
